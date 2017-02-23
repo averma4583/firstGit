bb85e9136509092c34616d5f3ec8a0a245be6d2a
averma4583 <averma4583@gmail.com>
2017-02-23 13:51:23 -0500

    first commit from eclipse to github

---
+++ src/firstGit/firstGit.java
@@ -0,0 +1,11 @@
+package firstGit;
+
+public class firstGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello GitHub!");
+	}
+
+}
